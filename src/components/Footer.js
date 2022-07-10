c5e7e957e3e9916676219960a848e14dc23cb71e
Stas <listat9027@gmail.com>
2022-07-11 01:20:43 +0300

    final vers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ import  Logo from '../assets/images/Logo.png'
 
 const Footer = () => {
   return (
-    <Box mt="80px" bgcolor="#ff2625">
+    <Box mt="80px" bgcolor="#d1cffa38">
       <Stack gap="40px" alignItems="center" px="40px"pt="25px" >
-          <img src={Logo}alt="logo" width="200px" height="112px"/>
-          <Typography variant="h5" pb="40px" mt='20px' color='#fff'>
+          <img src={Logo}alt="logo" width="170px" height="112px"/>
+          <Typography variant="h5" pb="40px" mt='20px' >
             Made by Listat
           </Typography>
       </Stack>
